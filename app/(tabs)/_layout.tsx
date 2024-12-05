8d22ead84e0a2c1626b01fd2c53d373aaf924e25
Juan Hernandez <jp.hernandez.yz@gmail.com>
2024-12-05 00:57:00 -0500

    App Skeleton Created

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,44 +1,37 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { Text } from 'react-native';
+import { Redirect, Tabs } from 'expo-router';
+import { useSession } from '@/context/ctx';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+export default function TabsLayout() {
+  const { session, isLoading } = useSession();
 
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+  // You can keep the splash screen open, or render a loading screen like we do here.
+  if (isLoading) {
+    return <Text>Loading...</Text>;
+  }
 
+  // Only require authentication within the (app) group's layout as users
+  // need to be able to access the (auth) group and sign in again.
+  if (!session) {
+    // On web, static rendering will stop here as the user is not authenticated
+    // in the headless Node process that the pages are rendered in.
+    return <Redirect href="/sign-in" />;
+  }
+
+  // This layout can be deferred because it's not the root layout.
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+    <Tabs>
       <Tabs.Screen
         name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
+        options={{headerTitle: "Home",}}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
+        name="search"
+        options={{headerTitle: "Search",}}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{headerTitle: "Profile",}}
       />
     </Tabs>
   );
--- app/(tabs)/search.tsx
+++ app/(tabs)/search.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function Index() {
+export default function Search() {
   return (
     <View
       style={{
@@ -9,7 +9,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>This is the home page.</Text>
+      <Text>This is the search page.</Text>
     </View>
   );
 }
