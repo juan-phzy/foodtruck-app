b7353cf679f48ed7816eff8c65a353c4f1af7b40
Juan Hernandez <jp.hernandez.yz@gmail.com>
2024-12-05 00:59:01 -0500

    minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,4 @@
-import { Stack, Slot } from "expo-router";
+import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SessionProvider } from "@/context/ctx";
