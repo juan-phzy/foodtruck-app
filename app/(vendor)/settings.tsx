bd0d18efd503b25d5509ab914c4fce3b9722e801
Juan Hernandez <jp.hernandez.yz@gmail.com>
2025-04-03 18:08:44 -0400

    upgraded rn

--- app/(vendor)/settings.tsx
+++ app/(vendor)/settings.tsx
@@ -1,4 +1,4 @@
-import { View, Text, TouchableOpacity } from "react-native";
+import { View } from "react-native";
 import { useClerk } from "@clerk/clerk-expo";
 import { router } from "expo-router";
 
